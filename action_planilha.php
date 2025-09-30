3958c69a88d824bc6a8653d210c654dbdd3cbda8
yurileuch <yuri.leuch@escola.pr.gov.br>
2025-09-30 14:32:56 -0300

    Update action_planilha.php

--- action_planilha.php
+++ action_planilha.php
@@ -2,7 +2,7 @@
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     // URL do Apps Script
-    $url = "COLE_AQUI_A_URL_DO_APPS_SCRIPT";
+    $url = "https://script.google.com/macros/s/AKfycbwFZGV6WEhv7J4-kNkRd9QL-LTM8jZfNgRqzNaLzKJJZR0HIVo2VoE9nHw1UHIOo9yR/exec";
 
     // Dados do formulário
     $data = [
